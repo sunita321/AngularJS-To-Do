3dbc3765a7254c22ddacb64ec7413156ba22aea7
Sunita Ramnarinesingh <sramnari@gmail.com>
2017-03-07 13:54:37 -0500

    Adding database mongoose connection dbconnection.js

--- server.js
+++ server.js
@@ -1,42 +1,25 @@
-// server.js
 
-// set up ========================
+
+// Dependencies ========================
 var express  = require('express');
 var app      = express();                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
+var logger = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-// configuration =================
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+var db = require('./db/dbconnection.js');
+var Todo = require('./db/todo.models.js');
 
+// configuration ======================
 
-//Mongoose connection ===================
-mongoose.Promise = Promise;
-mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost:27017/angularToDo');
-var db = mongoose.connection;
-
-// Show any mongoose errors
-db.on("error", function(error) 
-{
-  console.log("Mongoose Error: ", error);
-});
-
-//Mongoose connedtion to db
-db.once("open", function() 
-{
-  console.log("Mongoose connection successful!");
-});
-
-// Export the database
-module.exports = db;
+app.use(express.static(__dirname + '/app'));
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
 
 // Launch App
 var port = process.env.PORT || 3000;
